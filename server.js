48211c263bb2637940594715a1d8c3612c337787
Ayuan100 <369278452@qq.com>
2017-10-15 13:16:19 +0800

    debug comment

--- server.js
+++ server.js
@@ -61,27 +61,27 @@ mongoose.connect('mongodb://ayuan:ayuan@ds157444.mlab.com:57444/ayuan-first', fu
     else console.log('connect to mongodb');
 });
 
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('private.pem', 'utf8');
-var certificate = fs.readFileSync('file.crt', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+// var fs = require('fs');
+// var http = require('http');
+// var https = require('https');
+// var privateKey  = fs.readFileSync('private.pem', 'utf8');
+// var certificate = fs.readFileSync('file.crt', 'utf8');
+// var credentials = {key: privateKey, cert: certificate};
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-var PORT = 8080;
-var SSLPORT = 8081;
+// var httpServer = http.createServer(app);
+// var httpsServer = https.createServer(credentials, app);
+// var PORT = 8080;
+// var SSLPORT = 8081;
 
-httpServer.listen(PORT, function() {
-    console.log('HTTP Server is running on: http://localhost:%s', PORT);
-});
+// httpServer.listen(PORT, function() {
+//     console.log('HTTP Server is running on: http://localhost:%s', PORT);
+// });
 // httpsServer.listen(SSLPORT, function() {
 //     console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
 // });
 
 
-// var port = 8080;
-// app.listen(port, function(){
-// 	console.log("server listening on port ", port);
-// });
+var port = 8080;
+app.listen(port, function(){
+	console.log("server listening on port ", port);
+});
